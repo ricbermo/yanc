6756b5c1af048b7220d9e5aa1f1cba2d5eae67c1
Ricardo Berdejo <ricardo@fullstacklabs.co>
2022-05-10 12:37:52 -0500

    cleanup

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -33,17 +33,35 @@ local lsp_symbols = {
 }
 
 cmp.setup({
+  experimental = {
+    native_menu = false,
+    ghost_text = false,
+  },
+  confirmation = {
+    get_commit_characters = function()
+      return {}
+    end,
+  },
+  completion = {
+    completeopt = "menu,menuone,noinsert",
+    keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)]],
+    keyword_length = 1,
+  },
   sources = {
-    { name = "buffer" },
-    { name = "nvim_lsp" },
     { name = "luasnip" },
+    { name = "nvim_lsp" },
     { name = "path" },
+    { name = "buffer" },
     { name = "nvim_lsp_signature_help" }
   },
   mapping = {
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-q>"] = cmp.mapping.close(),
     ["<cr>"] = cmp.mapping.confirm({ select = true }),
-    ["<s-tab>"] = cmp.mapping.select_prev_item(),
-    ["<tab>"] = cmp.mapping.select_next_item()
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    ["<Tab>"] = cmp.mapping.select_next_item()
   },
   formatting = {
     format = function(entry, item)
@@ -58,5 +76,22 @@ cmp.setup({
       return item
     end
   },
-  snippet = { expand = function(args) luasnip.lsp_expand(args.body) end }
+  snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
+  preselect = cmp.PreselectMode.None,
+})
+
+cmp.setup.cmdline("/", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
 })
--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -3,9 +3,19 @@ if not telescope then return end
 
 telescope.setup {
   defaults = {
+    picker = {
+      hidden = false,
+    },
     vimgrep_arguments = {
-      "rg", "--color=never", "--no-heading", "--with-filename",
-      "--line-number", "--column", "--smart-case"
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--no-ignore",
+      "--smart-case",
+      "--hidden",
     },
     prompt_prefix = "  ",
     selection_caret = "  ",
@@ -26,7 +36,7 @@ telescope.setup {
       preview_cutoff = 120
     },
     file_sorter = require("telescope.sorters").get_fuzzy_file,
-    file_ignore_patterns = {},
+    file_ignore_patterns = { "node_modules", ".git/", "dist/", "build/" },
     generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
     path_display = { "absolute" },
     winblend = 0,
@@ -34,20 +44,28 @@ telescope.setup {
     borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
     color_devicons = true,
     use_less = true,
-    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+    set_env = { ["COLORTERM"] = "truecolor" },
     file_previewer = require("telescope.previewers").vim_buffer_cat.new,
     grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
     qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-    -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker
+    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+    mappings = {
+      i = {
+        ["<Tab>"] = "move_selection_next",
+        ["<S-Tab>"] = "move_selection_previous",
+      },
+      n = {
+        ["<Tab>"] = "move_selection_next",
+        ["<S-Tab>"] = "move_selection_previous",
+      },
+    },
   },
   extensions = {
     fzf = {
-      fuzzy = true, -- false will only do exact matching
-      override_generic_sorter = false, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case" -- or "ignore_case" or "respect_case"
-      -- the default case_mode is "smart_case"
+      fuzzy = true,
+      override_generic_sorter = false,
+      override_file_sorter = true,
+      case_mode = "smart_case"
     },
   }
 }
