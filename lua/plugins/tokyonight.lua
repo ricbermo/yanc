20f7d3e89393dbafb41653a52e7ac4d10371b55c
Ricardo Berdejo <ricardo@fullstacklabs.co>
2025-04-09 07:04:11 -0500

    fix color

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,8 +2,11 @@ return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
+  opts = {
+    style = "storm",
+  },
   init = function()
+    vim.o.background = "dark"
     vim.cmd [[colorscheme tokyonight]]
   end,
 }
